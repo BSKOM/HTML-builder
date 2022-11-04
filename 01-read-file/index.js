70a29bd44188154b701e039f6c76d5da88d4d514
BSKOM <b.shelestuaE@gmail.com>
2022-11-04 10:56:23 +0000

    feat: release output file to console

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePathIn = path.join(__dirname, './text.txt')
+
+let readStream = new fs.ReadStream(filePathIn, 'utf8');
+
+readStream.on('data', function (chunk) {console.log(chunk);});
+readStream.on('error', function (err) {
+  (err.code == 'ENOENT') ? console.log("file not found"):console.error(err);
+});
